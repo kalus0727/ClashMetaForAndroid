a27aee26e8bb8043df1a85fa0f7f304ffb8f6f77
GitHub Action <action@github.com>
2026-04-10 13:50:10 +0000

    Bump version to 3.0.2 (300002)

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "3.0.1"
-            versionCode = 300001
+            versionName = "3.0.2"
+            versionCode = 300002
 
             resValue("string", "release_name", "v$versionName")
             resValue("integer", "release_code", "$versionCode")
